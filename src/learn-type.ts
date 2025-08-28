f4d868e114b78e97b8bd516e8aa5c6c952f3f7d1
Rakib <rakib@Rakibs-MacBook-Pro.local>
2025-08-28 10:47:29 +0600

    basic typescript

---
+++ src/learn-type.ts
@@ -0,0 +1,9 @@
+let count: number = 34;
+let name :string = "Rakib"
+let fullName : string
+
+let amImad :boolean = false;
+let x:any=true;
+
+let multipleTypes : string | number = "Rakib";
+multipleTypes = 32;
